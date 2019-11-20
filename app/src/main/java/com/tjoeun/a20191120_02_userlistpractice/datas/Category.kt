0fd0663f91073bcc78a201db2041ad28cf51a4b3
조경진 <kj_cho@idealidea.co.kr>
2019-11-20 21:21:11 +0900

    리스트뷰 / 데이터클래스 / 서버API 생성

---
+++ app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/datas/Category.kt
@@ -0,0 +1,10 @@
+package com.tjoeun.a20191120_02_userlistpractice.datas
+
+class Category {
+
+    var id = 0
+    var title = ""
+    var color = ""
+
+
+}
---
+++ app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/datas/User.kt
@@ -0,0 +1,9 @@
+package com.tjoeun.a20191120_02_userlistpractice.datas
+
+class User {
+
+    var loginId = ""
+    var name = ""
+    var category:Category? = null
+
+}
