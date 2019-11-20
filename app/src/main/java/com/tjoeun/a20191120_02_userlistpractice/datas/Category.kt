a23fe40c8a9bf4e83089c6f75d741f96b438aea9
조경진 <kj_cho@idealidea.co.kr>
2019-11-20 21:51:08 +0900

    서버에서 사용자목록 받기 / 어댑터 기초 작업  => 다른프로젝트에서 복사 / 붙여넣기 기반으로

--- app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/datas/Category.kt
+++ app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/datas/Category.kt
@@ -1,10 +1,24 @@
 package com.tjoeun.a20191120_02_userlistpractice.datas
 
+import org.json.JSONObject
+
 class Category {
 
     var id = 0
     var title = ""
     var color = ""
+
+    companion object {
+        fun getCategoryFromJson(cateJson: JSONObject) : Category {
+            var categoryObject = Category()
+
+            categoryObject.id = cateJson.getInt("id")
+            categoryObject.title = cateJson.getString("title")
+            categoryObject.color = cateJson.getString("color")
+
+            return categoryObject
+        }
+    }
 
 
 }
--- app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/datas/User.kt
+++ app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/datas/User.kt
@@ -1,9 +1,32 @@
 package com.tjoeun.a20191120_02_userlistpractice.datas
 
+import org.json.JSONObject
+
 class User {
 
     var loginId = ""
     var name = ""
     var category:Category? = null
 
+    companion object {
+        fun getUserFromJson(userJson : JSONObject) : User {
+            var userObject = User()
+
+//            왼쪽 : 우리가만든 클래스 변수
+//            오른쪽 : 서버에서 내려주는 JSON 활용
+            userObject.loginId = userJson.getString("login_id")
+            userObject.name =  userJson.getString("name")
+
+            userObject.category = Category.getCategoryFromJson(userJson.getJSONObject("category"))
+
+            return userObject
+        }
+    }
+
 }
+
+
+
+
+
+
