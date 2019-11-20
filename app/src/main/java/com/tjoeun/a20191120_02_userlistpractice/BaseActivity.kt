35451a3558f89cd4ec707d73af73b26eb5718e3c
조경진 <kj_cho@idealidea.co.kr>
2019-11-20 20:54:05 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.tjoeun.a20191120_02_userlistpractice
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    var mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+
+}
