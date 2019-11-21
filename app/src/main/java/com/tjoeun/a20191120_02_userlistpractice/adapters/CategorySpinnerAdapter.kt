109895628a15469d187fc07d2bf79c94aae13cc3
조경진 <kj_cho@idealidea.co.kr>
2019-11-21 20:57:08 +0900

    XML 백그라운드 필터 변경

--- app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/adapters/CategorySpinnerAdapter.kt
+++ app/src/main/java/com/tjoeun/a20191120_02_userlistpractice/adapters/CategorySpinnerAdapter.kt
@@ -2,6 +2,7 @@ package com.tjoeun.a20191120_02_userlistpractice.adapters
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.PorterDuff
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -41,6 +42,8 @@ class CategorySpinnerAdapter(context:Context, res:Int, list:ArrayList<Category>)
 //        String => Color로 변경.
 //        categoryColorImg.setBackgroundColor(Color.parseColor(data.color))
 
+        categoryColorImg.background.setColorFilter(Color.parseColor(data.color), PorterDuff.Mode.SRC_ATOP)
+
         return row
 
     }
@@ -62,7 +65,7 @@ class CategorySpinnerAdapter(context:Context, res:Int, list:ArrayList<Category>)
         categoryTitleTxt.text = data.title
 
 //        String => Color로 변경.
-        categoryColorImg.setBackgroundColor(Color.parseColor(data.color))
+        categoryColorImg.background.setColorFilter(Color.parseColor(data.color), PorterDuff.Mode.SRC_ATOP)
 
         return row
     }
